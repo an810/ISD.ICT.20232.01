acf214e835d16e243fba51cdeb20ace7650e7e84
Hoang Anh <dohoanganh16032002@gmail.com>
2024-06-16 20:00:20 +0700

    Clear cart after pay

--- AIMS/Frontend/src/pages/Result.js
+++ AIMS/Frontend/src/pages/Result.js
@@ -1,87 +1,101 @@
 import Summary from "../components/Summary";
 import { Link } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { getItemFromLocalStorage, removeItemFromLocalStorage } from "../utils";
-import { useEffect } from "react";
+import { useEffect, useState, useContext } from "react";
+import { CartContext } from "../providers/CartContext";
 const Result = () => {
+  const { cartId } = useContext(CartContext);
   const location = useLocation();
+  const [order, setOrder] = useState({});
   const orderId = getItemFromLocalStorage("orderId");
-  const order = {};
   const queryParams = new URLSearchParams(location.search);
-  const vnp_Amount = queryParams.get('vnp_Amount');
+  const vnp_Amount = queryParams.get("vnp_Amount");
   // const vnp_BankCode = queryParams.get('vnp_BankCode');
   // const vnp_BankTranNo = queryParams.get('vnp_BankTranNo');
   // const vnp_CardType = queryParams.get('vnp_CardType');
-  const vnp_OrderInfo = queryParams.get('vnp_OrderInfo');
-  const vnp_PayDate = queryParams.get('vnp_PayDate');
-  const vnp_ResponseCode = queryParams.get('vnp_ResponseCode');
+  const vnp_OrderInfo = queryParams.get("vnp_OrderInfo");
+  const vnp_PayDate = queryParams.get("vnp_PayDate");
+  const vnp_ResponseCode = queryParams.get("vnp_ResponseCode");
   // const vnp_TmnCode = queryParams.get('vnp_TmnCode');
-  const vnp_TransactionNo = queryParams.get('vnp_TransactionNo');
-  const vnp_TransactionStatus = queryParams.get('vnp_TransactionStatus');
-  const vnp_TxnRef = queryParams.get('vnp_TxnRef');
+  const vnp_TransactionNo = queryParams.get("vnp_TransactionNo");
+  const vnp_TransactionStatus = queryParams.get("vnp_TransactionStatus");
+  const vnp_TxnRef = queryParams.get("vnp_TxnRef");
   // const vnp_TxnRef = queryParams.get('vnp_TxnRef');
   // const vnp_SecureHash = queryParams.get('vnp_SecureHash');
-  
+
   useEffect(() => {
-    axios.post("payment/save-payment-transaction",{
-      orderId: orderId,
-      vnp_ResponseCode: vnp_ResponseCode,
-      vnp_TransactionNo : vnp_TransactionNo,
-      vnp_Amount : vnp_Amount,
-      vnp_OrderInfo : vnp_OrderInfo,
-      vnp_TransactionStatus: vnp_TransactionStatus,
-      vnp_PayDate : vnp_PayDate,
-      vnp_TxnRef : vnp_TxnRef
-    }).then((response) => {
-      console.log(response.data);
-    }).catch((error) => {
-      console.log(error.data);
-    });
+    axios
+      .post("payment/save-payment-transaction", {
+        orderId: orderId,
+        vnp_ResponseCode: vnp_ResponseCode,
+        vnp_TransactionNo: vnp_TransactionNo,
+        vnp_Amount: vnp_Amount,
+        vnp_OrderInfo: vnp_OrderInfo,
+        vnp_TransactionStatus: vnp_TransactionStatus,
+        vnp_PayDate: vnp_PayDate,
+        vnp_TxnRef: vnp_TxnRef,
+      })
+      .then((response) => {
+        axios
+          .get(`order/${orderId}`)
+          .then((response) => {
+            setOrder(response.data.data);
+          })
+          .catch((error) => {
+            console.log(error.data);
+          });
 
-    // get order by orderId
-    axios.get(`order/${orderId}`).then((response) => {
-      order = response.data.data;
-    }).catch((error) => {
-      console.log(error.data);
-    });
+        const invoice = {
+          order,
+          paymentTransaction: {
+            transactionId: "TRANS67890",
+            orderId: "ORD54321",
+            errorCode: "00",
+            amount: 100000,
+            transactionNum: "TXN789",
+            transactionContent: "Payment for order #ORD54321",
+            message: "Transaction successful",
+            createdAt: "2023-06-14T10:30:00Z",
+            vnpTxnRef: "TXNREF123",
+          },
+        };
 
-    const invoice = {
-      invoiceId: 'INV12345',
-      order,
-      paymentTransaction: {
-        transactionId: 'TRANS67890',
-        orderId: 'ORD54321',
-        errorCode: '00',
-        amount: 100000,
-        transactionNum: 'TXN789',
-        transactionContent: 'Payment for order #ORD54321',
-        message: 'Transaction successful',
-        createdAt: '2023-06-14T10:30:00Z',
-        vnpTxnRef: 'TXNREF123'
-      }
-    };
+        axios
+          .post("invoice/create", invoice)
+          .then((response) => {
+            if (vnp_ResponseCode === "00") {
+              axios
+                .post(`cart/${cartId}/clear`)
+                .then((response) => {
+                  removeItemFromLocalStorage("orderId");
+                })
+                .catch((error) => {
+                  console.log(error.data);
+                });
+            }
+          })
+          .catch((error) => {
+            console.log(error.data);
+          });
+      })
+      .catch((error) => {
+        console.log(error.data);
+      });
+  }, [
+    orderId,
+    vnp_ResponseCode,
+    vnp_TransactionNo,
+    vnp_Amount,
+    vnp_OrderInfo,
+    vnp_TransactionStatus,
+    vnp_PayDate,
+    order,
+    vnp_TxnRef,
+    cartId,
+  ]);
 
-    axios.post("invoice/create", invoice).then((response) => {
-      console.log(response.data);
-    }).catch((error) => {
-      console.log(error.data);
-    } );
-  }, [orderId, vnp_ResponseCode, vnp_TransactionNo, vnp_Amount, vnp_OrderInfo, vnp_TransactionStatus, vnp_PayDate])
-
-  // // clear cart if the result is successful
-  // if (vnp_ResponseCode === "00") {
-  //   axios
-  //     .post(`cart/${cartId}/clear`)
-  //     .then((response) => {
-  //       console.log(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.data);
-  //     });
-  // }
-
-  
   return (
     <div>
       <div className="flex px-40 justify-between shadow-sm py-10">
@@ -97,7 +111,9 @@ const Result = () => {
           <div className=" font-bold my-10 text-xl w-full items-center justify-center flex flex-col">
             <div className="flex flex-col justify-center items-center mt-40">
               <div className="text-6xl	">PAYMENT RESULT</div>
-              <div className="text-2xl mt-10">{vnp_ResponseCode === "00" ? "Sucessfull" : "Failed"}</div>
+              <div className="text-2xl mt-10">
+                {vnp_ResponseCode === "00" ? "Sucessfull" : "Failed"}
+              </div>
             </div>
 
             <div className="flex mt-40">
@@ -108,7 +124,6 @@ const Result = () => {
               </Link>
             </div>
           </div>
-          <Summary />
         </div>
       </div>
     </div>
